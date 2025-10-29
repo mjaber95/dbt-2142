8776821e1155c855be4bc27994e934fe5f3f1dbf
Mamdouh Jaber <mjaber995@icloud.com>
2025-10-29 14:22:44 +0000

    added tests to schema

---
+++ models/intermediate/merged.sql
@@ -0,0 +1,9 @@
+SELECT 
+    product_id,
+    date_date,
+    quantity,
+    stock,
+    price
+FROM {{ref("stg_raw_data_circle__raw_cc_sales")}}
+JOIN {{ref("stg_raw_data_circle__raw_cc_stock")}}
+USING (product_id)
